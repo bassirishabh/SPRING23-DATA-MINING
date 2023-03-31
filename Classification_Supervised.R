bcc4ef3aef54673a4385a7dbf2e1b85aa00342d6
sankalpc7 <alexStraut@7798>
2023-03-31 03:08:53 -0500

    adding models file

--- Classification_Supervised.R
+++ Classification_Supervised.R
@@ -1,29 +1,20 @@
+library(rstudioapi)
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+rm(list=ls())
+
 # Load data
-load("~/Desktop/TAMU/Study/Spring23/DataMining/Project/R_Project/class_data.RData")
-library(e1071)
-# Split data into training and testing sets
-set.seed(123)
-train_idx <- sample(nrow(x), 0.8*nrow(x))
-x_train <- x[train_idx, ]
-y_train <- y[train_idx]
-x_test <- x[-train_idx, ]
-y_test <- y[-train_idx]
-# 
-# 
-x_train <- as.matrix(x_train)
-y_train <- as.matrix(y_train)
-x_test <- as.matrix(x_test)
-y_test <- as.matrix(y_test)
-logit_fit <- glm(y_train ~ x_train)
-# logit_cv <- glm(x_train, y_train, family = binomial(link = "logit"), type.measure = "class", K = 5)
-# logit_best <- logit_fit # In logistic regression, we don't need to tune hyperparameters
+library(ISLR)
+library(MASS)
+load("class_data.RData")
 
-logit_pred <- predict(logit_fit, as.data.frame(x_test))
-
-# Convert probabilities to labels
-ynew <- as.numeric(logit_pred > 0.5)
-
-# Compute test error
-ynew_test <- as.numeric(y_test == 1)
-test_error <- mean(ynew != ynew_test)
-save(ynew, test_error, file = "~/Desktop/TAMU/Study/Spring23/DataMining/Project/R_Project/output.rdata")
+## K-Nearest Neighbors
+library(class)
+?knn
+x=data.frame(x, y)
+set.seed(1)
+train=sample(1:nrow(x),250) # by default sample is without replacement
+# X=Carseats[,c(2,3,4,5,6,8,9)]#extract the continuous covariates
+knn.pred=knn(x[train,],x[-train,],y[train],k=50)
+table(knn.pred,y[-train])
+mean(knn.pred==y[-train])
---
+++ q1_v2.R
@@ -0,0 +1,84 @@
+library(rstudioapi)
+library(dplyr)
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+rm(list=ls())
+
+# Load data
+library(ISLR)
+library(MASS)
+library(tree)
+library(randomForest)
+# library(caret)
+load("class_data.RData")
+
+# Make training and test set
+y_up=ifelse(y==0,"No","Yes")
+y_up = as.factor(y_up)
+# x <- as.data.frame(scale(x))
+# x <- log(as.data.frame(x))
+# process <- preProcess(as.data.frame(x), method=c("range"))
+# x <- predict(process, as.data.frame(x))
+x=data.frame(x, y_up)
+
+# Logistic regression
+set.seed(1)
+train=sample(1:nrow(x),250) # by default sample is without replacement
+glm.fit=glm(y_up~., data=x,family=binomial, subset=train)
+glm.probs=predict(glm.fit,newdata=x[-train,],type="response") 
+glm.pred=ifelse(glm.probs >0.5,"Yes","No")
+y_up.test=x$y_up[-train]
+table(glm.pred,y_up.test)
+mean(glm.pred==y_up.test)
+
+## Linear Discriminant Analysis
+lda.fit=lda(y_up~.,data=x, subset=train)
+lda.fit
+x.test=x[-train,]
+lda.pred=predict(lda.fit,x.test)
+table(lda.pred$class,x.test$y_up)
+mean(lda.pred$class==x.test$y_up)
+
+# ## Quadratic Discriminant Analysis
+# qda.fit=qda(y_up~.,data=x, subset=train)
+# qda.fit
+# x.test=x[-train,]
+# qda.pred=predict(qda.fit,x.test)
+# table(qda.pred$class,x.test$y_up)
+# mean(qda.pred$class==x.test$y_up)
+
+## SVM
+library(e1071)
+?svm
+svmfit=svm(y_up~.,data=x,subset=train, kernel="radial", cost=.1)
+svm.pred=predict(svmfit, x[-train,])
+table(svm.pred, y_up[-train])
+mean(svm.pred==y_up[-train])
+
+## RF
+# Lets fit a random forest and see how well it performs. 
+# We will use the response `medv`, the median housing value (in \$1K dollars)
+# rf.boston=randomForest(y_up~.,data=x,subset=train,mtry=500,ntree=500)
+# pred=predict(fit,x[-train,])
+# some_err <- 1 - mean(pred == x[-train,]$y_up)
+# rf.boston
+# 
+# oob.err=double(60)
+# test.err=double(60)
+# for(mtry in 1:60){
+#   fit=randomForest(y_up~.,data=x,subset=train,mtry=mtry,ntree=500)
+#   oob.err[mtry]= fit$err.rate[500]#Mean squared error for 400 trees
+#   pred=predict(fit,x[-train,])
+#   # print(pred)
+#   test.err[mtry] <- 1 - mean(pred == x[-train,]$y_up)
+# }
+# matplot(1:mtry,cbind(test.err,oob.err),pch=19,col=c("red","blue"),type="b",ylab="Mean Squared Error")
+# legend("topright",legend=c("Test", "OOB"),pch=19,col=c("red","blue"))
+
+
+#With boosting, the number of trees is a tuning parameter, and if we have too many we can overfit. 
+#We use cross-validation to choose the number of trees
+boost.boston=gbm(y_up~.,data=x[train,],distribution="gaussian",n.trees=10000,shrinkage=0.001,interaction.depth=9,cv.fold=10)
+n.trees.cv=gbm.perf(boost.boston, method = "cv")
+gbmpred=predict(boost.boston,newdata=x[-train,],n.trees=n.trees.cv)
+boost_err = 1-mean(gbmpred == x[-train,]$y_up)
