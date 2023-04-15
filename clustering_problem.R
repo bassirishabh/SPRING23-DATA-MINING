a7802b10be2f8fab4c45349fef8d8209e33894f9
sankalpc7 <alexStraut@7798>
2023-04-15 16:56:13 -0500

    pushing changes for clustering problems

--- clustering_problem.R
+++ clustering_problem.R
@@ -1,6 +1,9 @@
-# CLusering Problem - 2
+# Clusering Problem - 2
 
-# Clean up
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+rm(list=ls())
+
 library(rstudioapi)
 library(dplyr)
 library(cluster)
@@ -8,9 +11,14 @@ library(NbClust)
 library(MVN)
 library(ggplot2)
 library("factoextra")
-current_path = rstudioapi::getActiveDocumentContext()$path 
-setwd(dirname(current_path ))
-rm(list=ls())
+library(mclust)
+library(doParallel)
+library(dbscan)
+
+# setup parallel backend
+ncores = parallel::detectCores()
+cl = makeCluster(ncores)
+registerDoParallel(cl)
 
 set.seed(1234)
 
@@ -26,7 +34,8 @@ bet <- (n-p-3)/(2*(n-p-1))
 a   <- p/2
 b   <- (n-p-1)/2
 
-mvn(y) # performs Shapiro-Wilk test for multivariate normality
+# result <- mvn(data = setosa, mvnTest = "hz", silent = TRUE)
+# result$multivariateNormality
 
 # Plot beta and u quantiles
 y.std <- scale(y)
@@ -49,7 +58,15 @@ variance_explained <- cumsum(pca$sdev^2 / sum(pca$sdev^2))
 
 # Identify the number of principal components required to explain 90% or 99% of the variance
 n_components_90 <- which(variance_explained >= 0.9)[1]
+n_components_95 <- which(variance_explained >= 0.95)[1]
 n_components_99 <- which(variance_explained >= 0.99)[1]
+
+y_pca_90 <- pca$x[,1:n_components_90]
+y_pca_95 <- pca$x[,1:n_components_95]
+y_pca_99 <- pca$x[,1:n_components_99]
+
+pc.ind = c(n_components_90, n_components_95, n_components_99)
+PCs.sub = lapply(pc.ind, function(x) pca$x[,1:x])
 
 #kmeans # use Calinski and Harabasz (1974) ch index
 ch.index = function(x,kmax,iter.max=100,nstart=25)
@@ -69,16 +86,21 @@ ch.index = function(x,kmax,iter.max=100,nstart=25)
   return(list(k=2:kmax,ch=ch, sil = silwid))
 }
 
-ch = lapply(list(pca$x[,1:n_components_90], pca$x[,1:n_components_99]), ch.index, kmax=25)
+ch = lapply(PCs.sub, ch.index, kmax=25)
 
 # Plotting CH and silhoutte results
 ch_90 <- ch[[1]]
-ch_99 <- ch[[2]]
+ch_95 <- ch[[2]]
+ch_99 <- ch[[3]]
 
 # Extract variables for plotting
 k_90 <- ch_90$k
 ch_val_90 <- ch_90$ch
 sil_score_90 <- ch_90$sil
+
+k_95 <- ch_95$k
+ch_val_95 <- ch_95$ch
+sil_score_95 <- ch_95$sil
 
 k_99 <- ch_99$k
 ch_val_99 <- ch_99$ch
@@ -87,6 +109,8 @@ sil_score_99 <- ch_99$sil
 # Find the maximum values for ch_val and sil_score
 max_ch_val_90 <- k_90[which.max(ch_val_90)]
 max_sil_score_90 <- k_90[which.max(sil_score_90)]
+max_ch_val_95 <- k_95[which.max(ch_val_95)]
+max_sil_score_95 <- k_95[which.max(sil_score_95)]
 max_ch_val_99 <- k_99[which.max(ch_val_99)]
 max_sil_score_99 <- k_99[which.max(sil_score_99)]
 
@@ -94,30 +118,38 @@ max_sil_score_99 <- k_99[which.max(sil_score_99)]
 plot(k_90, ch_val_90, type = "o", pch = 16, col = "blue", 
      ylim = range(ch_val_90, ch_val_99), xlab = "k values", ylab = "ch_val", 
      main = "ch_val vs k values")
+lines(k_95, ch_val_95, type = "o", pch = 16, col = "black")
 lines(k_99, ch_val_99, type = "o", pch = 16, col = "red")
 points(max_ch_val_90, ch_val_90[which.max(ch_val_90)], pch = 16, col = "green")
+points(max_ch_val_95, ch_val_95[which.max(ch_val_95)], pch = 16, col = "green")
 points(max_ch_val_99, ch_val_99[which.max(ch_val_99)], pch = 16, col = "green")
 abline(v = max_ch_val_90, col = "black", lty = 2)
+abline(v = max_ch_val_95, col = "black", lty = 2)
 abline(v = max_ch_val_99, col = "black", lty = 2)
-legend("topright", legend = c("90", "99"), col = c("blue", "red"), pch = c(16, 16))
+legend("topright", legend = c("90", "95", "99"), col = c("blue","black", "red"), pch = c(16, 16, 16))
 
 # Create the second plot for sil_score
 plot(k_90, sil_score_90, type = "o", pch = 16, col = "blue", 
      ylim = range(sil_score_90, sil_score_99), xlab = "k values", ylab = "sil_score",
      main = "sil_score vs k values")
+lines(k_99, sil_score_95, type = "o", pch = 16, col = "red")
 lines(k_99, sil_score_99, type = "o", pch = 16, col = "red")
 points(max_sil_score_90, sil_score_90[which.max(sil_score_90)], pch = 16, col = "green")
+points(max_sil_score_95, sil_score_95[which.max(sil_score_95)], pch = 16, col = "green")
 points(max_sil_score_99, sil_score_99[which.max(sil_score_99)], pch = 16, col = "green")
 abline(v = max_sil_score_90, col = "black", lty = 2)
+abline(v = max_sil_score_95, col = "black", lty = 2)
 abline(v = max_sil_score_99, col = "black", lty = 2)
-legend("topright", legend = c("90", "99"), col = c("blue", "red"), pch = c(16, 16))
+legend("topright", legend = c("90", "95", "99"), col = c("blue","black", "red"), pch = c(16, 16, 16))
                                                                                            
 
 # NB Clustering
-nbc_90 <- NbClust(pca$x[,1:n_components_90], distance = "euclidean", min.nc = 2, 
+nbc_90 <- NbClust(y_pca_90, distance = "euclidean", min.nc = 2, 
               max.nc = 6, method = "complete", index = "all")
-nbc_99 <- NbClust(pca$x[,1:n_components_99], distance = "euclidean", min.nc = 2, 
+nbc_99 <- NbClust(y_pca_99, distance = "euclidean", min.nc = 2, 
                   max.nc = 6, method = "complete", index = "all")
+
+stopCluster(cl)
 
 fviz_nbclust <- function (x, FUNcluster = NULL, method = c("silhouette", "wss", 
                                                            "gap_stat"), diss = NULL, k.max = 10, nboot = 100, verbose = interactive(), 
@@ -220,19 +252,119 @@ fviz_nbclust <- function (x, FUNcluster = NULL, method = c("silhouette", "wss",
   }
 }
 
-fviz_nbclust(nc_90)
-fviz_nbclust(nc_99)
+fviz_nbclust(nbc_90)
+fviz_nbclust(nbc_99)
 
 
 # K is finalized, Now we perform Different Clustering Methods
 
 ### Kmeans
+set.seed(123)
+km.res <- kmeans(y_pca_99, 3, nstart = 25)
+# print(km.res)
 
-### TSNE
+fviz_cluster(km.res, data = y_pca_99,
+             palette = c("#2E9FDF", "#00AFBB", "#E7B800"), 
+             geom = "point",
+             ellipse.type = "convex", 
+             ggtheme = theme_bw()
+)
 
-### GMM
+### Gaussian mixture model (GMM)
+library(mclust)
+gmm = Mclust(PCs.sub[[3]], G = 3)
+plot(PCs.sub[[3]],col=gmm$classification,cex=2,pch=1,lwd=2)
+
 
 ### Hierarchical Clustering
+# Function to get within and between cluster sum of squares from hclust
+clust.SS = function(data, cluster){
+  
+  twss = vector()
+  tss = vector()
+  bss = vector()
+  wss = list()
+  cluster = as.matrix(cluster)
+  
+  for(j in seq_len(ncol(cluster))){
+    ss = aggregate(data, 
+                   by=list(cluster[,j]), 
+                   function(x) sum(scale(x, scale=F)**2))
+    wss[[j]] = rowSums(ss[,-1])
+    twss[j] = sum(ss[,-1])
+    tss[j] = sum(scale(data, scale=F)**2)
+    bss[j] = tss[j] - twss[j]
+  }
+  
+  ss.all = list(tss=tss, wss=wss, twss=twss, bss=bss)
+  
+  return(ss.all) 
+  
+}
+
+#function for computing CH index
+ch.index2 = function(data, kmax, twss, bss){
+  
+  ch = numeric(length=kmax-1)
+  n = nrow(data)
+  for (k in 2:kmax) {
+    to = twss[k-1]
+    b = bss[k-1]
+    ch[k-1] = (b/(k-1))/(to/(n-k))
+  }
+  return(list(k=2:kmax,ch=ch))
+}
+
+# Get distances for all subsets of the PC space considered
+dists = lapply(PCs.sub, dist)
+
+hc.complete = lapply(dists, hclust, method="complete")
+hc.single   = lapply(dists, hclust, method="single")
+hc.average  = lapply(dists, hclust, method="average")
+
+clusts = list(hc.complete, hc.single, hc.average)
+
+# Function to plot CH and Total WSS for hclust across linkages, pc components kept, and values of K
+plot.ch.wss = function(ch.list, wss.list, linkage){
+  par(mfrow=c(2,3), mai=c(.4,.5,.4,.2))
+  for(i in 1:3){
+    data = ch.list[[i]]
+    plot(data$k, data$ch, ylab="", xlab="", xaxt="n")
+    axis(1, labels=F)
+    abline(v = which.max(data$ch)+1, col=2)
+    if(i==1) title(ylab="CH Index", line=2, font.lab=2, cex.lab=1.2)
+    title(main=paste(pc.ind[i], "Components Kept"), line = .5)
+  }
+  for(i in 1:3){
+    data = wss.list[[i]]
+    plot(2:25, data$twss, ylab="", xlab="")
+    title(xlab = "K", font.lab=2, line = 2)
+    if(i==1) title(ylab="Total WSS", line=2, cex.lab=1.2, font.lab=2)
+  }
+  title(paste("CH Index and Total WSS For", linkage,  "Linkage Clustering Across PC Spaces"), outer=T, line=-1)
+}
+
+
+methods = c("Complete", "Single", "Average")
+
+for(method in seq_along(clusts)){
+  print(method)
+  cuts = lapply(clusts[[method]], function(x) cutree(x,k=2:25))
+  ss = mapply(clust.SS, cluster=cuts, data=list(y_pca_90, y_pca_99), SIMPLIFY = F)
+  ch = lapply(ss, function(x) ch.index2(data=y, kmax=25, twss=x$twss, bss=x$bss))
+  pdf(paste0("ch2_", methods[method], ".pdf"))
+  plot.ch.wss(ch, ss, linkage=methods[method])
+  dev.off()
+}
 
 ### DBSCAN
+kNNdistplot(PCs.sub[[3]], k=4)
+abline(h=35, col="red")
+set.seed(1234)
+db = hdbscan(PCs.sub[[3]], minPts = 3)
+
+kNNdistplot(PCs.sub[[3]], k=4)
+abline(h=40, col="green")
+set.seed(1234)
+db = hdbscan(PCs.sub[[3]], minPts = 3)
 
